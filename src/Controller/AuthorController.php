8936dc00551914447d2dae1f372cde90df26356f
Badiaa Bouhdid <badiaabouhdid@gmail.com>
2024-09-23 15:00:48 +0100

    list author

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -8,9 +8,12 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class AuthorController extends AbstractController
 {   private $authors;
-   public function __construct(){
-
-   }
+    public function __construct(){
+            $this->authors=[
+                ['id'=>1, 'name'=>'Taha Hussain','nbrBooks'=>300,'picture'=>'images/th.jpeg'],
+                ['id'=>2, 'name'=>'Victor Hugo','nbrBooks'=>200,'picture'=>'images/vh.jpeg'],
+            ];
+    }
    #[Route("/library",name:"app_library",methods:["GET"])]
     public function index(){
        return $this->render('author/index.html.twig');
