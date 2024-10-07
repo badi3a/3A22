8f6254beaf082da8919c8f9064e6d29dca5cb3ff
Badiaa Bouhdid <badiaabouhdid@gmail.com>
2024-10-07 14:46:25 +0100

    add author done

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -10,8 +10,8 @@ class AuthorController extends AbstractController
 {   private $authors;
     public function __construct(){
             $this->authors=[
-                ['id'=>1, 'name'=>'Taha Hussain','nbrBooks'=>300,'picture'=>'images/th.jpeg'],
-                ['id'=>2, 'name'=>'Victor Hugo','nbrBooks'=>200,'picture'=>'images/vh.jpeg'],
+                ['id'=>1, 'name'=>'Taha Hussain','nbrBooks'=>300,'picture'=>'images/th.jpeg', 'email'=>"taha@gmail.com"],
+                ['id'=>2, 'name'=>'Victor Hugo','nbrBooks'=>200,'picture'=>'images/vh.jpeg','email'=>"vh@gmail.com"],
             ];
     }
    #[Route("/library",name:"app_library",methods:["GET"])]
@@ -34,9 +34,17 @@ class AuthorController extends AbstractController
    #[Route("/list",name:"app_list",methods:["GET"])]
    public function authorList(){
 
-       $this->render('author/list.html.twig',
+       return $this->render('author/list.html.twig',
        [
            'authors'=>$this->authors
        ]);
    }
+
+    #[Route("/showDetails/{id}",name:"app_showDetail",methods:["GET"])]
+public function showDetailsAction($id){
+        //show author details in showDetails.html.twig
+        //var_dump($id);
+        //die();
+        return $this->render("author/showDetails.html.twig", array());
+    }
 }
